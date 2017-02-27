4fc75083a47e0eb2e469418d7b2b29feab7ce244
Ethan Bruins <brudut93@gmail.com>
2017-02-27 14:38:09 +0000

    EnemyPrefab working

--- Take 2/Assets/Resources/EnemyController.cs
+++ Take 2/Assets/Resources/EnemyController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+    [SerializeField]
     private GameObject ship;
     public GameObject Ship
     {
@@ -10,21 +11,76 @@ public class EnemyController : MonoBehaviour {
         get { Debug.LogError("Paul sucks giant dick"); return ship;  }
     }
 
+    [SerializeField]
+    private float distanceToShip;
 
-	// Use this for initialization
-	void Start()
-    {
+    [SerializeField]
+    private float rotationSpeed;
 
-	}
-	
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float nextShot;
+
+    [SerializeField]
+    private float shotInterval;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private Transform bulletSpawn;
+
+    [SerializeField]
+    private float bulletSpeed;
+
 	// Update is called once per frame
 	void Update()
     {
-        MoveTowardsShip();
+        EnemyFunctions();
 	}
 
-    void MoveTowardsShip()
+    public void EnemyFunctions()
     {
+        Move();
+        Rotate();
+        Shoot();
+    }
 
+    void Move()
+    {
+        float distance = Vector2.Distance(ship.transform.position, transform.position);
+
+        if (distance > distanceToShip)
+        {
+            Vector3 myPosition = transform.position;
+            Vector3 direction = ship.transform.position - transform.position;
+            direction.Normalize();
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.RotateAround(ship.transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void Rotate()
+    {
+        Vector2 direction = ship.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+    }
+
+    void Shoot()
+    {
+        if(Time.time > nextShot)
+        {
+            nextShot = Time.time + shotInterval;
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            Vector2 direction = ship.transform.position - transform.position;
+            direction.Normalize();
+            bullet.gameObject.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+        }
     }
 }

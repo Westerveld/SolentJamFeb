d5681a9f1cdd2fb7b2af4371f679318e0d739596
Julian Bath <2bathj70@W65151.solent.ac.uk>
2017-02-27 14:20:18 +0000

    Made the thrusters move the ship, yo

--- Take 2/Assets/Scripts/ThrusterController.cs
+++ Take 2/Assets/Scripts/ThrusterController.cs
@@ -1,16 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ThrusterController : MonoBehaviour
 {
     [SerializeField]
     private GameObject engine;
-
     [SerializeField]
     private float rotationSpeed;
+    [SerializeField]
+    private float force;
+    [SerializeField]
+    private float maxSpeed;
 
-    void Update()
+    [SerializeField]
+    private string joystick;
+
+    void FixedUpdate()
     {
-        engine.transform.RotateAround(transform.position, Vector3.back, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        engine.transform.RotateAround(transform.position, Vector3.back, Input.GetAxis("Horizontal" + joystick) * rotationSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetButton("Activate" + joystick))
+        {
+            Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+            float engineAngle = engine.transform.rotation.eulerAngles.z;
+            //rigidBody.AddForceAtPosition(new Vector2(Mathf.Cos(engineAngle), Mathf.Sin(engineAngle)) * force, engine.transform.position, ForceMode2D.Force);
+            rigidBody.AddForce((transform.position - engine.transform.position).normalized * force * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            if (rigidBody.velocity.magnitude > maxSpeed)
+            {
+                rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+            }
+        }
     }
 }

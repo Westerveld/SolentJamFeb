bddb66eb6651a213e86504423a8854ec84f8a5d6
Paul Hanlon <2hanlp05@W65141.solent.ac.uk>
2017-02-27 13:33:48 +0000

    GameManger WIP, Yo

---
+++ Take 2/Assets/Resources/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+public class GameManager : MonoBehaviour {
+
+    int score;
+
+    //Enemy/Wake managment
+    int waveNumber = 0;
+    int waveSize;
+    public const int initalWaveSize = 10;
+    public const int waveSizeIncrement = 5;
+    float nextWaveDelaySeconds = 10.0f;
+ 
+
+    //List enemies
+    List<Transform> enemyList = new List<Transform>();
+
+    //<A>collection of enemies
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void UpdateWave()
+    {
+
+    }
+
+    void NextWave()
+    {
+        waveNumber++;
+        waveSize = 10 + waveNumber * waveSizeIncrement;
+        for (int i = 0; i < waveSize; waveSize++ )
+        {
+
+        }
+    }
+
+    //If Ship Dead 
+    void EndGame()
+    {
+        
+    }
+
+}

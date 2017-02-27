3a4f121fb64b241e59a8886136b8bf7d196ef30f
Julian Bath <2bathj70@W65151.solent.ac.uk>
2017-02-27 13:41:43 +0000

    Added thrusters that rotate around the ship, yo

---
+++ Take 2/Assets/Scripts/ThrusterController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrusterController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject engine;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    void Update()
+    {
+        engine.transform.RotateAround(transform.position, Vector3.back, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+    }
+}

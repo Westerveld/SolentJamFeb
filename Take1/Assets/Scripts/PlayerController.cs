904630ad78750c3fa4763ff5185a2a535c49d152
Julian Bath <2bathj70@W65181.solent.ac.uk>
2017-02-27 11:53:43 +0000

    Added player prefab with controller script

---
+++ Take1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float turnSpeed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime);
+    }
+}

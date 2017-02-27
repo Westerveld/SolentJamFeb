1c6949b59e1d51bb0c3e23dd0876fc48c440f7f8
Ethan Bruins <brudut93@gmail.com>
2017-02-27 12:07:25 +0000

    Yes

---
+++ Take1/Assets/Scripts/GoalController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Team
+{
+     RedTeam, BlueTeam
+}
+public class GoalController : MonoBehaviour {
+
+    [SerializeField]
+    private Team mTeam;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

459491ce295fd10bd1951faae5c415b1e404996b
Paul Hanlon <2hanlp05@solent.ac.uk>
2017-02-28 13:13:44 +0000

    Updated main menu, added a Music manager Yo

---
+++ Take 2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+  public  void BTN_Begin()
+    {
+
+    }
+
+    public void BTN_HowToPlay()
+    {
+
+    }
+
+    public void BTN_Credits()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+}
--- Take 2/Assets/Scripts/MusicManager.cs
+++ Take 2/Assets/Scripts/MusicManager.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum MusicState
+{
+    MainMenu,
+    InGame,
+    InGameIntense
+}
+
 public class MusicManager : MonoBehaviour {
 
+    public AudioSource Base;
+    public AudioSource wave;
+    public AudioSource intense;
+    float musicTime;
 	// Use this for initialization
 	void Start () {
-	
+        DontDestroyOnLoad(transform.gameObject);
+        UpdateMusic(MusicState.MainMenu);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (Input.GetKeyDown(KeyCode.A)) UpdateMusic(MusicState.MainMenu);
+
+        if (Input.GetKeyDown(KeyCode.S)) UpdateMusic(MusicState.InGame);
+
+        if (Input.GetKeyDown(KeyCode.D)) UpdateMusic(MusicState.InGameIntense);
+
+
+    }
+
+   public void UpdateMusic(MusicState ms)
+    {
+        switch (ms)
+        {
+            case MusicState.MainMenu:
+                Base.mute = false;
+                wave.mute = true;
+                intense.mute = true;
+                break;
+            case MusicState.InGame:
+                Base.mute = false;
+                wave.mute = false;
+                intense.mute = true;
+                break;
+            case MusicState.InGameIntense:
+                Base.mute = false;
+                wave.mute = false;
+                intense.mute = false;
+                break;
+            default:
+                break;
+        }
+    }
+
+
 }

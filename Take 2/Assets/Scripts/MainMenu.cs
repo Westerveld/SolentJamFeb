c10652147f67eaf1636e45286a16fdfb9c57e72b
Paul Hanlon <2hanlp05@solent.ac.uk>
2017-03-01 12:31:09 +0000

    Updated main menu

--- Take 2/Assets/Scripts/MainMenu.cs
+++ Take 2/Assets/Scripts/MainMenu.cs
@@ -5,23 +5,29 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
     [SerializeField]
     private string beginLevelName = "PaulTesting";
-  public  void BTN_Begin()
+    [SerializeField]
+    private GameObject HowToPlay;
+    [SerializeField]
+    private GameObject Credits;
+    public  void BTN_Begin()
     {
         SceneManager.LoadScene(beginLevelName);
     }
 
     public void BTN_HowToPlay()
     {
-
+        HowToPlay.SetActive(true);
+        Credits.SetActive(false);
     }
 
     public void BTN_Credits()
     {
-
+        Credits.SetActive(true);
+        HowToPlay.SetActive(false);
     }
 
     public void Exit()
     {
-
+        Application.Quit();
     }
 }
--- Take 2/Assets/Scripts/MusicManager.cs
+++ Take 2/Assets/Scripts/MusicManager.cs
@@ -19,11 +19,6 @@ public class MusicManager : MonoBehaviour {
         GameManager.GameStateChanged += UpdateMusic;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-    }
-
     void UpdateMusic(GameState gs)
     {
         switch (gs)

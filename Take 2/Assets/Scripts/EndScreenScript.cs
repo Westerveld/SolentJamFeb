f1e1a47bbb7d8573d3909eb7a32cfe56c8b92be3
Paul Hanlon <2hanlp05@solent.ac.uk>
2017-03-01 14:12:10 +0000

    EndScene

---
+++ Take 2/Assets/Scripts/EndScreenScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class EndScreenScript : MonoBehaviour {
+    [SerializeField]
+    private Text scoreText;
+
+    void Start()
+    {
+        scoreText.text = PlayerPrefs.GetInt("FinalScore").ToString();
+        PlayerPrefs.SetInt("FinalScore", -666);
+        PlayerPrefs.Save();
+    }
+
+
+	public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ReplayLevel()
+    {      
+      SceneManager.LoadScene("PaulTesting");
+    }
+}

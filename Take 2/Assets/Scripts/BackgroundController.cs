1be868b0a97ac4be03e35741bb9cd0c40595ed9b
2bathj70 <2bathj70@W65154.solent.ac.uk>
2017-03-02 14:41:09 +0000

    Added scrolling background, yo

---
+++ Take 2/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 parentPosition = transform.parent.position;
+        transform.localPosition = new Vector3(-parentPosition.x % 20, -parentPosition.y % 20, -parentPosition.z % 20);
+    }
+}
